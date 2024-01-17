d9757186a469b1858111db02a253dd4ad9aa6fe6
Karthik Boddapati <boddapatikarthik12.com>
2024-01-17 18:10:38 +0530

    local packages setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Karthik Boddapati',
+    author_email= 'boddapati1999@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
